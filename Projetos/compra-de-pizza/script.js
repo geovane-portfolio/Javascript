0332f9ec421e6c700c07cb2739c5ebd347add3c1
Geovane de Sá Ferreira <geovane.portfolio@gmail.com>
2021-07-13 10:31:56 -0300

    Modal
    adicionado funções ao modal

--- Projetos/compra-de-pizza/script.js
+++ Projetos/compra-de-pizza/script.js
@@ -1,7 +1,11 @@
+let modalQt = 1;
+let cart = [];
+let modalKey = 0;
+
 const c = (el)=>document.querySelector(el);
 const cs = (el)=>document.querySelectorAll(el);
 
-// Preenchendo os elementos
+// Listagem das Pizzas
 pizzaJson.map( (item, index) => {
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
 
@@ -17,14 +21,25 @@ pizzaJson.map( (item, index) => {
         //closest, está saindo de 'a' e selecionado o mais proximo que tenha .pizza-item.
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
 
+        modalQt = 1;
+        modalKey = key;
+    
+
         //preechendo o modal
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
         c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+        c('.pizzaInfo--size.selected').classList.remove('selected');//remove o selected
+
         cs('.pizzaInfo--size').forEach( (size, sizeIndex)=> {// add peso 
+            if(sizeIndex == 2) { //limpa e sempre deixa a grande selecionada
+                size.classList.add('selected');
+            }
             size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
-        })
+        });
+
+        c('.pizzaInfo--qt').innerHTML = modalQt;
 
         c('.pizzaWindowArea').style.opacity = 0;
         c('.pizzaWindowArea').style.display = 'flex';// exibir o modal
@@ -36,4 +51,38 @@ pizzaJson.map( (item, index) => {
     c('.pizza-area').append( pizzaItem );
 });
 
-//
+// Eventos do modal
+
+function closeModal() {
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout( ()=> {
+        c('.pizzaWindowArea').style.display = 'none';
+    },500);
+}
+
+cs('.pizzaInfo--cancelButton, .pizzaInfo--cancelMobileButton').forEach((item)=>{
+    item.addEventListener('click', closeModal);
+});
+
+c('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
+    if(modalQt > 1) {
+    modalQt--;
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+    }
+});
+c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
+    modalQt++;
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+});
+
+cs('.pizzaInfo--size').forEach( (size, sizeIndex)=> {// add peso 
+    size.addEventListener('click', (e)=>{
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    });
+});
+
+c('.pizzaInfo--addButton').addEventListener('click', ()=>{
+    //qual a pizza, tamanho e quantidade
+
+});
